589b210d1196cfa1ab90fbaa2d1f561e51ebc7f0
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-03-15 14:53:38 -0400

    Added Commands + Added Terminal Logic + Styled 404 Page

--- js/main.js
+++ js/main.js
@@ -2,33 +2,58 @@
 document.getElementById("useragent").innerText = navigator.userAgent;
 document.getElementById("date").innerText = new Date().toString();
 
+// Dynamic Age Calculation in "intro.txt"
 setInterval(() => {
     let time = (new Date() - new Date(1106812800000)) / 31556952000; // unix time of birth / milliseconds per year
-    document.getElementById("age").innerHTML = time.toString().substring(0, 12);
+    for (let i = 0; i < document.getElementsByClassName("age").length; i++) {
+        document.getElementsByClassName("age")[i].innerHTML = time.toString().substring(0, 12);
+    }
 }, 50);
+
+let history = [];
+let historypos = 0;
 
 const projects_text = "BBBBBBBBBBBBBBBBBBB";
 
-const about_text = "AAAAA<u>BBBBBBB</u>AAAAAAAAAAAAAAAAA<span>*asdasd*</span>wewewewe";
+const about_text = "asd";
 
 const intro_text =
     "<i>Hello!</i><br>" +
-    "I'm Aniket Chadalavada, a <span id='age'></span>-year-old full-stack developer based in Cumming, Georgia.<br><br>" +
+    "I'm Aniket Chadalavada, a <span class='age'></span>-year-old full-stack developer based in Cumming, Georgia.<br><br>" +
     "I am:<br>" +
     "* A junior at <a href='https://www.forsyth.k12.ga.us/alliance' target='_blank' rel='noopener'>Alliance Academy for Innovation</a>.<br>" +
-    "* Lead Developer for <a href='https://www.avengerrobotics.org/' target='_blank' rel='noopener'>FIRST Tech Challenge Team 14892.</a><br>" +
-    "* Developer for <a href='https://www.avengerrobotics.org/' target='_blank' rel='noopener'>FIRST Robotics Competition Team 7451.</a><br>" +
+    "* The Lead Developer for <a href='https://www.avengerrobotics.org/' target='_blank' rel='noopener'>FIRST Tech Challenge Team 14892.</a><br>" +
+    "* A Developer for <a href='https://www.avengerrobotics.org/' target='_blank' rel='noopener'>FIRST Robotics Competition Team 7451.</a><br>" +
     "To learn more about me run <span class='typedlink' onclick='usertype(\"cat about.txt\", output, about_text);' style='cursor: pointer;'><u>cat about.txt</u></span>, to read about my projects run <span class='typedlink' onclick='usertype(\"cat projects.txt\", output, projects_text);' style='cursor: pointer;'><u>cat projects.txt</u></span>.<hr>" +
-    "This portfolio is based on the UI of Windows Powershell and the functions of a classic Unix terminal.";
+    "This portfolio is based on the functions of a classic Unix terminal. Run <span class='typedlink' onclick='usertype(\"help\", help, 0);' style='cursor: pointer;'><u>help</u></span> for a list of valid commands.<br>" +
+    "While this website emulates a terminal and is primarily text-and-input based, it is still a website. <span class='typedlink' onclick='usertype(\"echo Hello World!\", output, \"Hello World!\");' style='cursor: pointer;'><u>Underlined text is clickable</u></span>.<br>" +
+    "<i>This website is still in development.</i>";
 
 const secret_text = "\"/(DES)/LEWKEN\"<br>" +
-    "Hint: 2π";
+    "Hint: 2π Vigenere";
+
+const logo_text = "Toasty Terminal<br>" +
+    "Pasteleft ThatCrispyToast. No lefts unlimited.<br><br>" +
+    "User Agent: USERAGENT<br>" +
+    "Time: TIME<br>"
 
 const files = {
     "about.txt": about_text,
     "intro.txt": intro_text,
     "projects.txt": projects_text,
-    "secret.txt": secret_text
+    "secret.txt": secret_text,
+    "logo.txt": logo_text
+}
+
+const commands = {
+    "cat [filename.txt]": "Outputs the content of a file to the terminal.",
+    "clear": "Clears the terminal window.",
+    "echo [text]": "Outputs [text] to the terminal.",
+    "exit": "Closes the terminal window.",
+    "help": "Outputs a list of valid commands, their parameters, and their definitions to the terminal.",
+    "ls": "Outputs a list of files present in the immediate directory.",
+    "start [url]": "Opens a new browser window at /[url] or [url.com].",
+
 }
 
 // Run Terminal Animation On Window Load
@@ -75,6 +100,7 @@ function generateCursor(lbr = true) {
     document.getElementById("text-window").scrollTop = document.getElementById("text-window").scrollHeight;
 }
 
+/* -----KEY HANDLING----- */
 // Block Enter Key
 document.onkeydown = function (e) {
     if (e.key === "Enter") {
@@ -82,13 +108,28 @@ document.onkeydown = function (e) {
     }
 }
 
-// Detect and Check Input on "Enter"
+// Detect and Check Input on "Enter" + Handle Arrow Key Command Movement
 document.getElementById("userinput").addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
+        historypos = 0;
+        history.push(document.getElementById("userinput").innerHTML);
         checkInput()
         document.getElementById("userinput").innerHTML = "";
+    } else if (event.key === "ArrowUp") {
+        if (history.length - historypos > 0) {
+            historypos++;
+            document.getElementById("userinput").innerHTML = history[history.length - historypos];
+        }
+    } else if (event.key === "ArrowDown") {
+        if (history.length - historypos < history.length - 1) {
+            historypos--;
+            document.getElementById("userinput").innerHTML = history[history.length - historypos];
+        } else {
+            document.getElementById("userinput").innerHTML = "";
+        }
     }
 });
+/* ------------------------ */
 
 function ls() {
     let out = "";
@@ -96,6 +137,14 @@ function ls() {
         out += `<span class='typedlink' onclick='usertype(\"cat ${filesKey}\", output, files[\"${filesKey}"]);' style='cursor: pointer;'><u>${filesKey}</u></span> `;
     }
     output(out);
+}
+
+function help() {
+    let out = "------------<br>";
+    for (const commandsKey in commands) {
+        out += commandsKey + " :|: " + commands[commandsKey] + "<br>";
+    }
+    output(out + "------------");
 }
 
 // Read Input from Terminal and Generate Appropriate Output
@@ -107,21 +156,27 @@ function checkInput() {
         if (file) {
             output(file);
         } else {
-            output("File not found. Run <span class='typedlink' onclick='usertype(\"ls\", ls, 0);' style='cursor: pointer;'><u>ls</u></a>");
+            output("File not found. Run <span class='typedlink' onclick='usertype(\"ls\", ls, 0);' style='cursor: pointer;'><u>ls</u></span>");
         }
-
+    } else if (uiHTML.startsWith("echo")) {
+        output(uiHTML.substring(uiHTML.indexOf(" ") + 1));
+    } else if (uiHTML.startsWith("exit")) {
+        window.close();
     } else if (uiHTML.startsWith("start")) {
         open(uiHTML.substring(uiHTML.indexOf(" ") + 1));
-
+        generateCursor(false);
     } else if (uiHTML.startsWith("ls")) {
         ls()
-
+    } else if (uiHTML.startsWith("clear")) {
+        document.getElementById("maincontent").innerHTML = "";
+        document.getElementById("logo").innerHTML = "";
+        generateCursor(false);
+    } else if (uiHTML.startsWith("help")) {
+        help();
+    } else if (uiHTML === "") {
+        generateCursor(false);
     } else {
-        if (uiHTML === "") {
-            generateCursor(false);
-        } else {
-            output("Command not recognized. Run help for a list of valid commands.");
-        }
-
+            output("Command not recognized. Run <span class='typedlink' onclick='usertype(\"help\", help, 0);' style='cursor: pointer;'><u>help</u></span> for a list of valid commands.");
     }
+
 }
